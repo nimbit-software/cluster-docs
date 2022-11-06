1e020bc9518bc382b6a8640a4865eedfc5c18574
David Sooter <david.sooter@cpars.de>
2022-11-06 22:09:56 +0100

    Deploy website - based on 0b200d7efb848965d1bb4c2ca6d6574250d7baaa

---
+++ assets/js/cefeb96b.da2bea70.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcluster_docs=self.webpackChunkcluster_docs||[]).push([[570],{1240:s=>{s.exports=JSON.parse('{"label":"Argocd","permalink":"/cluster-docs/docs/tags/argocd","allTagsPath":"/cluster-docs/docs/tags","count":1,"items":[{"id":"managed-cluster/argocd","title":"Argocd","description":"","permalink":"/cluster-docs/docs/managed-cluster/argocd"}]}')}}]);
